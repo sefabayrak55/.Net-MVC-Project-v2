ac47433749789ad2be4cfb1baefc7ee449e0bff2
sefabayrak55 <sefabayrak55@hotmail.com>
2022-12-17 16:37:29 +0300

    Login and Register Page created

---
+++ day-09/ProductApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,65 @@
+﻿using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductApp.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AuthenticationController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([FromForm] RegisterDto model)
+        {
+            if(ModelState.IsValid)
+            {
+                var applicationUser = new ApplicationUser()
+                {
+                    UserName = model.UserName,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email
+                };
+
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
